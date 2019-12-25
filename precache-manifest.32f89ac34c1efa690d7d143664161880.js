e2d617575fecf56146add1678099fe5846343b01
Eric <eeoneric@gmail.com>
2019-12-24 21:28:31 -0500

    Updates

---
+++ precache-manifest.32f89ac34c1efa690d7d143664161880.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8db5bcba1b9c0010607b8d9ba6cc413b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84fb44cff0681eed19a0",
+    "url": "/static/css/main.1e09a291.chunk.css"
+  },
+  {
+    "revision": "5d2f9f9183beefea64eb",
+    "url": "/static/js/2.ebe24981.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ebe24981.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84fb44cff0681eed19a0",
+    "url": "/static/js/main.a6f52140.chunk.js"
+  },
+  {
+    "revision": "689f42e3afe5e7ad1b83",
+    "url": "/static/js/runtime-main.4f8f3b2f.js"
+  },
+  {
+    "revision": "5a87cce84010f7cea085ae218d44a64b",
+    "url": "/static/media/NotoSans-Regular.5a87cce8.ttf"
+  },
+  {
+    "revision": "aae67cbad289417108cf642c8660496a",
+    "url": "/static/media/splatterprofile.aae67cba.png"
+  }
+]);
